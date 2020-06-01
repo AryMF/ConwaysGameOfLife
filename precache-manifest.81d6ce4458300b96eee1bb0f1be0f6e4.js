c145ad6b882a1eef90d55fdbe18d34c481c7d308
AryMF <ariadne.hp@gmail.com>
2020-05-31 23:23:32 -0500

    Updates

---
+++ precache-manifest.81d6ce4458300b96eee1bb0f1be0f6e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ca59b84e9e5115a15651912b4395a2",
+    "url": "/ConwaysGameOfLife/index.html"
+  },
+  {
+    "revision": "ff3435556b66a3b2a40d",
+    "url": "/ConwaysGameOfLife/static/js/2.dce7bbbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ConwaysGameOfLife/static/js/2.dce7bbbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "471e4a258d47f648399d",
+    "url": "/ConwaysGameOfLife/static/js/main.796c79a1.chunk.js"
+  },
+  {
+    "revision": "b79ee4d91e3c44d5169e",
+    "url": "/ConwaysGameOfLife/static/js/runtime-main.a72fbecc.js"
+  },
+  {
+    "revision": "9304e47b449d10ff0e10fb4c59da9910",
+    "url": "/ConwaysGameOfLife/static/media/logoSVG.9304e47b.svg"
+  }
+]);
